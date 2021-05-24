0bab5234c389859d32eaec00dcfe62e7da1d8a7a
滑 <1395348685z@gmail.com>
2021-05-24 20:45:02 +0800

    More build configurations

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,9 @@
-module.exports = {
+const common = {
   entry: './src/index.js',
   output: {
     path: `${__dirname}/dist`,
-    filename: 'pjax.js',
-    library: {
-      name: 'Pjax',
-      type: 'global',
-      export: 'default',
-    },
   },
-  mode: 'development',
+  devtool: 'source-map',
   module: {
     rules: [
       {
@@ -22,3 +16,64 @@ module.exports = {
     ],
   },
 };
+
+module.exports = [
+  {
+    ...common,
+    name: 'global-dev',
+    output: {
+      ...common.output,
+      filename: 'pjax.js',
+      library: {
+        name: 'Pjax',
+        type: 'global',
+        export: 'default',
+      },
+    },
+    mode: 'development',
+  },
+  {
+    ...common,
+    name: 'global-prod',
+    output: {
+      ...common.output,
+      filename: 'pjax.min.js',
+      library: {
+        name: 'Pjax',
+        type: 'global',
+        export: 'default',
+      },
+    },
+    mode: 'production',
+  },
+  {
+    ...common,
+    name: 'module-dev',
+    output: {
+      ...common.output,
+      filename: 'pjax.esm.js',
+      library: {
+        type: 'module',
+      },
+    },
+    mode: 'development',
+    experiments: {
+      outputModule: true,
+    },
+  },
+  {
+    ...common,
+    name: 'module-prod',
+    output: {
+      ...common.output,
+      filename: 'pjax.esm.min.js',
+      library: {
+        type: 'module',
+      },
+    },
+    mode: 'production',
+    experiments: {
+      outputModule: true,
+    },
+  },
+];
